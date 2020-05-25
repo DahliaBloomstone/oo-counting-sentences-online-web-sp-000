f4cf49b29f37a5e1810bb648410bcf173585351f
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-25 21:22:00 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,6 +17,6 @@ class String
   def count_sentences
     sentence_array = self.split(/[.?!]/)
     sentence_array.delete_if{|sentence| sentence.empty?}
-    sentence_array.lengthself.split?(/[.?!])/)
+    sentence_array.length
   end
 end
